62df78b12c52922c27d6ee8f4c2edf500deea990
TeeXee19 <teec3000@gmail.com>
2020-11-30 16:05:39 +0100

    Working on Authentication

---
+++ src/components/Login.vue
@@ -0,0 +1,14 @@
+<template>
+	<div class="container">
+		<div  class="row justify-content-center">
+			<div class="col-md-5">
+				<div class="card">
+					<div class="card-header">Login</div>
+					<div class="card-body">
+						<div></div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
