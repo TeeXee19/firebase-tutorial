919fdd964ae11880c259747fee7c6c770c437b5f
TeeXee19 <teec3000@gmail.com>
2020-11-30 16:29:56 +0100

    Added the authentication to the login page

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -5,10 +5,50 @@
 				<div class="card">
 					<div class="card-header">Login</div>
 					<div class="card-body">
-						<div></div>
+						<div v-if="error" class="alert alert-danger">{{error}}</div>
+						<form action="#" @submit.prevent="submit">
+							<div class="form-group">
+								<label for="email" class=" col-form-label text-md-right">Email</label>
+								<input type="email" id="email" class="form-control" value required autofocus v-model="form.email">
+							</div>
+							<div class="form-group">
+								<label for="password" class=" col-form-label text-md-right">Password</label>
+								<input type="password"  id="password" class="form-control" required v-model="form.password">
+							</div>
+							<div class="form-group row mb-0">
+								<button type="submit" class="btn btn-primary">Login</button>
+							</div>
+						</form>
 					</div>
 				</div>
 			</div>
 		</div>
 	</div>
 </template>
+
+<script >
+	import { auth } from "../firebaseDb";
+
+	export default {
+		data() {
+			return {
+				form: {
+					email: '',
+					password: ''
+				},
+				error: null
+			};
+		},
+		methods: {
+			submit() {
+				auth.signInWithEmailAndPassword(this.form.email, this.form.password)
+				.then(data => {
+					this.$route.replace({name: 'Dashboard'});
+				})
+				.catch(err => {
+					this.error = err.message;
+				});
+			}
+		}
+	}
+</script>
--- src/firebaseDb.js
+++ src/firebaseDb.js
@@ -18,3 +18,4 @@ firebase.initializeApp(firebaseConfig);
 
 
 export const db = firebase.firestore(); 
+export const auth = firebase.auth(); 
